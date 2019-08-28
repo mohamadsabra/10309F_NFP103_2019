0d45d4db3b23c454305b0b977cf6bacd62058be2
mosabra <mosabra@MOSABRA-LAP.lb.oracle.com>
2019-08-28 19:31:25 +0300

    Creation de la Classe Serveur

--- CNAMChatting/src/Serveur.java
+++ CNAMChatting/src/Serveur.java
@@ -1,4 +1,30 @@
+import java.net.ServerSocket;
+import java.net.Socket;
 
+/*
+ * Un serveur de discussion qui fournit des messages et des fichiers publics et privés.
+ */
 public class Serveur {
 
+	//Socket Serveur
+	private static ServerSocket serverSocket = null;
+	//Socket Client
+	private static Socket clientSocket = null;
+	
+	public static ArrayList<ThreadClient> clients = new ArrayList<ThreadClient>();
+	
+	public static void main(String args[]) {
+		
+		int numeroDuPort = 1234;
+		
+		
+		if (args.length < 1) 
+		{System.out.println("Aucun port spécifié par l'utilisateur. \\nLe serveur est en cours d'exécution avec le numéro de port par défaut=" + numeroDuPort);} 
+		else 
+		{
+			numeroDuPort = Integer.valueOf(args[0]).intValue();
+			System.out.println("Le serveur est en cours d'exécution avec le numéro de port spécifié=" + numeroDuPort);
+		}
+		
+	}
 }
